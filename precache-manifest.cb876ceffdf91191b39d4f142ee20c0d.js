a78e320e7cfdb6f841747cbbc4df1b491cec3606
Juan arango <jd.arango@uniandes.edu.co>
2020-05-18 15:14:35 -0500

    Updates

---
+++ precache-manifest.cb876ceffdf91191b39d4f142ee20c0d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9eb2807518a1be794b7d8df4b56e5834",
+    "url": "/osmandappReport/index.html"
+  },
+  {
+    "revision": "7be609675cb50c6ffdec",
+    "url": "/osmandappReport/static/css/main.c1d6f021.chunk.css"
+  },
+  {
+    "revision": "5451a7c6cfecb7db40fe",
+    "url": "/osmandappReport/static/js/2.f143ec26.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/osmandappReport/static/js/2.f143ec26.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7be609675cb50c6ffdec",
+    "url": "/osmandappReport/static/js/main.0dce8d3d.chunk.js"
+  },
+  {
+    "revision": "b04c480cbccc3f36280f",
+    "url": "/osmandappReport/static/js/runtime-main.31537adb.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/osmandappReport/static/media/logo.5d5d9eef.svg"
+  }
+]);
