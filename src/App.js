fd5b86ff51452c0bc7ab543e26093b7b7f62fd12
Juan arango <jd.arango@uniandes.edu.co>
2020-05-18 15:48:44 -0500

    f

--- src/App.js
+++ src/App.js
@@ -2,20 +2,17 @@ import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import Home from "./Components/Home";
-import { Switch, Route, BrowserRouter as Router } from "react-router-dom";
+import { Switch, Route, withRouter } from "react-router-dom";
 function App() {
   return (
-    <Router>
-      <div className="App">
-        <Switch>
-          <Route path="" exact component={Home} />
-        </Switch>
-        <Switch>
-          <Route path="osmandappReport/home" component={Home} />
-        </Switch>
-      </div>
-    </Router>
+    <div className="App">
+      <Switch>
+        <Route path="/" exact component={Home} />
+
+        <Route path="osmandappReport/home" component={Home} />
+      </Switch>
+    </div>
   );
 }
 
-export default App;
+export default withRouter(App);
