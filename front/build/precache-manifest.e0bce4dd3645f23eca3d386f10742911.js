4da52f2caa9276838101e2142e7baab42c7472ae
Juan arango <jd.arango@uniandes.edu.co>
2020-05-19 18:51:36 -0500

    j

---
+++ front/build/precache-manifest.e0bce4dd3645f23eca3d386f10742911.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "62dccdc4f4cb4a8b7904bfca842a0539",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d2fad231391d63f74a28",
+    "url": "/static/css/main.c1d6f021.chunk.css"
+  },
+  {
+    "revision": "ac771100ddbc9788fb1f",
+    "url": "/static/js/2.1c3f9db3.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.1c3f9db3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d2fad231391d63f74a28",
+    "url": "/static/js/main.04704af9.chunk.js"
+  },
+  {
+    "revision": "6dab95c898cc010282c2",
+    "url": "/static/js/runtime-main.eb05f368.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
